2b614c6c16f77d91053f78784c6719bf05a80feb
Vaibhav2468 <129250214+Vaibhav2468@users.noreply.github.com>
2026-03-28 14:27:56 +0530

    Update config.js

--- student-frontend/src/config.js
+++ student-frontend/src/config.js
@@ -1,5 +1,5 @@
 // Vite compatible API base URL
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+//const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 // Production backend URL (commented out)
  const API_BASE_URL = "https://rms-backend-a7kl.onrender.com";
